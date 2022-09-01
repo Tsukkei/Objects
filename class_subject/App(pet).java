d23dbe6c9d403186adbdfd0de9f829488dcf6ec6
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-09-01 16:08:52 +0800

    Create App(pet).java

---
+++ class_subject/App(pet).java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static void main(String[] args){
+        pets Wolf = new pets("Ranga", 10, 9, true);
+        Wolf.sayMyPets();
+
+        pets dragon = new pets("Tio", 10, 10, true);
+        dragon.sayMyPets();
+
+        pets dog = new pets("Inuarashi", 9, 9, true);
+        dog.sayMyPets();
+
+    }
+
+
+}
